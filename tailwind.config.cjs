448423a4ddaba6ce5a071eea4600a0fa64a18700
Justin Lam <justin.kh.lam@gmail.com>
2023-02-01 14:35:44 -0500

    add in font family and screen breakpoint sizing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,6 +19,18 @@ module.exports = {
           "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(85,9,121,1) 35%, rgba(0,212,255,1) 100%);",
         "mobile-home": "url('./assets/HomePageGraphic.png')",
       }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
 };
