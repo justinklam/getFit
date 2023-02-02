e0f3f9f5d46aba45d6b37c0a62d2d91dc23f3084
Justin Lam <justin.kh.lam@gmail.com>
2023-02-02 00:52:00 -0500

    add in additional props. add in onClickHandler

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,15 +1,24 @@
+// AnchorLink
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
 };
 
-const Link = ({ page }: Props) => {
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
   // regEx replaces all empty spaces
   const lowerCasePage = page.toLowerCase().replace(/ /g, "");
 
   return (
-    <AnchorLink href={`#${lowerCasePage}`} onClick={}>
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-primary-500" : ""
+      } transition duration-500 hover:text-primary-300`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
       {page}
     </AnchorLink>
   );
