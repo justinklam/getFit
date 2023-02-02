15d24f538b673df3664d64f29a2d0fe9b9e5e2c2
Justin Lam <justin.kh.lam@gmail.com>
2023-02-02 00:28:23 -0500

    add in additional elements for links

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -12,7 +12,26 @@ const Navbar = (props: Props) => {
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6 opacity-5`}>
-        <div className={`${flexBetween} mx-auto w-5/6`}></div>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} gap-15 w-full`}>
+            {/* Left Side */}
+            <img src={Logo} alt="logo" />
+
+            {/* Right Side */}
+            <div className={`${flexBetween} w-full`}>
+              {/* Links */}
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <p>Home</p>
+                <p>Benefits</p>
+                <p>Classes</p>
+                <p>Contact Us</p>
+              </div>
+
+              {/* Sign-In */}
+              <div></div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
