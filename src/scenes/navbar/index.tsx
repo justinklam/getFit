2a01b2fe3d448f89874584231fa5c9f516148f79
Justin Lam <justin.kh.lam@gmail.com>
2023-02-04 17:09:14 -0500

    add in conditional rendering for modal. add in styling for base modal container

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -75,6 +75,10 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+      {/* Mobile Modal */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl"></div>
+      )}
     </nav>
   );
 };
