3ada6533a69e79a339f5b764d35d74503a18ff2a
Justin Lam <justin.kh.lam@gmail.com>
2023-02-05 00:50:00 -0500

    add in mobile modal links. add in styling for vertical menu

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -84,6 +84,29 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
               <XMarkIcon className="h-6 w-6 text-gray-20" />
             </button>
           </div>
+          {/* Menu Items */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
         </div>
       )}
     </nav>
