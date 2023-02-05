19794535d529bf72c2f5a8a53e22ffa52e6a8447
Justin Lam <justin.kh.lam@gmail.com>
2023-02-05 00:53:14 -0500

    add in a useState for navBar top of page state

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
   return (
     <div className="app bg-gray-20">
