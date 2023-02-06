e55812d771e1b1107c70ee817a6aedb94ed1edfe
Justin Lam <justin.kh.lam@gmail.com>
2023-02-06 15:51:26 -0500

    import image assets into index.tsx. import helper functions and shared buttons

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,4 +1,13 @@
 import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import useMediaQuery from "@/hooks/useMediaQuery";
+
+// Image assets
+import HomePageText from "@/assets/text-no-bg.png";
+import HomePageGraphic from "@/assets/homePageGraphic.jpeg";
+import Sponser1 from "@/assets/sponser-1.png";
+import Sponser2 from "@/assets/sponser-2.png";
+import Sponser3 from "@/assets/sponser-3.png";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
