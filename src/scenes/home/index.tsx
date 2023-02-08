48d9837a87432c86404233fc62620cbfaf96540f
Justin Lam <justin.kh.lam@gmail.com>
2023-02-08 00:58:03 -0500

    add styling for sponser section

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -69,8 +69,8 @@ const Home = ({ setSelectedPage }: Props) => {
       {/* Sponsers & Partners */}
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
-          <div>
-            <div>
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
               <img src={Sponser1} alt="sponser-1" />
               <img src={Sponser2} alt="sponser-2" />
               <img src={Sponser3} alt="sponser-3" />
