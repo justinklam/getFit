308311e81786171252ddeced9b138f12dce2a49d
Justin Lam <justin.kh.lam@gmail.com>
2023-02-11 00:57:26 -0500

    initialize benefit component with base styling for main container

---
+++ src/scenes/benefits/Benefit.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+
+const Benefit = (props: Props) => {
+  return (
+    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"></div>
+  );
+};
+
+export default Benefit;
