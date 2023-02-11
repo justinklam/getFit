32292119c08920aca972c718631e73ac67cf68c8
Justin Lam <justin.kh.lam@gmail.com>
2023-02-10 22:17:23 -0500

    add in gym benefits description

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -22,6 +22,26 @@ const Benefits = ({ setSelectedPage }: Props) => {
       >
         <div>
           <HText>A GYM FOR EVERYONE</HText>
+          <p>
+            Welcome to our one-of-a-kind gym where we offer a unique combination
+            of bouldering, yoga, and weightlifting!
+            <br></br>
+            <br></br>
+            Whether you're an experienced climber looking for new challenges or
+            a beginner just starting out, our bouldering wall provides a fun and
+            safe environment for climbers of all skill levels.
+            <br></br>
+            Our yoga studio offers a peaceful escape from the hustle and bustle
+            of the gym, with a variety of classes taught by experienced
+            instructors.
+            <br></br>
+            And for those looking to build strength, our weightlifting area is
+            equipped with the latest equipment, allowing you to achieve your
+            fitness goals with ease.
+            <br></br>
+            Come and experience the perfect blend of physical challenge,
+            mindfulness, and strength-building at our gym!
+          </p>
         </div>
       </motion.div>
     </section>
