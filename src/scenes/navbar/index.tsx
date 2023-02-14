425279c7f9e29991507592b70d2175b9c2375885
Justin Lam <justin.kh.lam@gmail.com>
2023-02-14 00:33:58 -0500

    adjust navbar for classes title change

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -50,7 +50,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
                     setSelectedPage={setSelectedPage}
                   />
                   <Link
-                    page="Our Classes"
+                    page="Classes"
                     selectedPage={selectedPage}
                     setSelectedPage={setSelectedPage}
                   />
@@ -103,7 +103,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
             <Link
-              page="Our Classes"
+              page="Classes"
               selectedPage={selectedPage}
               setSelectedPage={setSelectedPage}
             />
