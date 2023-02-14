8418f2d9df9e4654193b20beef1d2303c3b5987d
Justin Lam <justin.kh.lam@gmail.com>
2023-02-14 00:37:31 -0500

    import in ourClasses component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SelectedPage } from "@/shared/types";
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -38,6 +39,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
