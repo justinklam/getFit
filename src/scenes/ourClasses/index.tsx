2b69360e7a7d961f5d5aec4706a05d131c49f821
Justin Lam <justin.kh.lam@gmail.com>
2023-02-16 00:29:31 -0500

    code reorganization

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -12,10 +12,6 @@ import classes2 from "@/assets/classes-2.jpeg";
 import classes4 from "@/assets/classes-4.jpeg";
 import classes5 from "@/assets/classes-5.jpeg";
 
-type Props = {
-  setSelectedPage: (value: SelectedPage) => void;
-};
-
 const classes: Array<ClassType> = [
   {
     name: "Yoga",
@@ -42,6 +38,10 @@ const classes: Array<ClassType> = [
     image: classes5,
   },
 ];
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
 const OurClasses = ({ setSelectedPage }: Props) => {
   return (
