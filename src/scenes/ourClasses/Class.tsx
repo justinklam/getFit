4393636ccf97c4794bfa412c70d13bf1925a88dd
Justin Lam <justin.kh.lam@gmail.com>
2023-02-16 00:24:32 -0500

    add in styling for main slider

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -5,9 +5,11 @@ type Props = {
 };
 
 const Class = ({ name, description, image }: Props) => {
+  const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0`;
+
   return (
     <li className="in-line-block relative mx-5 h-[380px] w-[450px]">
-      <div>
+      <div className={overlayStyles}>
         <p>{name}</p>
         <p>{description}</p>
       </div>
