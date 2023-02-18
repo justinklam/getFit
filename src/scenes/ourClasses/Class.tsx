72868428197bdb42f15cbd9093875fdbfef7d81f
Justin Lam <justin.kh.lam@gmail.com>
2023-02-18 00:18:53 -0500

    add styling for images

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -13,7 +13,11 @@ const Class = ({ name, description, image }: Props) => {
         <p className="text-2xl">{name}</p>
         <p className="mt-5">{description}</p>
       </div>
-      <img src={image} alt={`${image}`} />
+      <img
+        className="h-full w-[450px] object-cover"
+        src={image}
+        alt={`${image}`}
+      />
     </li>
   );
 };
