2b2b540343a88f1a435c3b1ba9f5ec971c37f201
Justin Lam <justin.kh.lam@gmail.com>
2023-02-18 00:29:01 -0500

    import dependencies. add in props for setSelectedPage

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,6 +1,12 @@
-type Props = {};
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
 
-const ContactUs = (props: Props) => {
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
   return <div>ContactUs</div>;
 };
 
