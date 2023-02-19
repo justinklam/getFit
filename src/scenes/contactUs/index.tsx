aab44cf9cd8454dd467d6ff3a0408da63d582176
Justin Lam <justin.kh.lam@gmail.com>
2023-02-19 14:39:38 -0500

    add in animation for header

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -14,7 +14,21 @@ const ContactUs = ({ setSelectedPage }: Props) => {
     <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
-      ></motion.div>
+      >
+        {/* Header */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          // triggers when 50% of viewport is in view
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 }, // starting position
+            visible: { opacity: 1, x: 0 }, // end position
+          }}
+        ></motion.div>
+      </motion.div>
     </section>
   );
 };
