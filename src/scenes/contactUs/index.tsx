0123d7d4833a6afe48775d781b5317419e672656
Justin Lam <justin.kh.lam@gmail.com>
2023-02-19 14:04:48 -0500

    import contactUs graphic

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -2,6 +2,9 @@ import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
 
+// Image assets
+import ContactUsGraphic from "@/assets/contactUs.jpeg";
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
