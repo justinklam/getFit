cbf467c3a742bce88de94477e504013175974180
Justin Lam <justin.kh.lam@gmail.com>
2023-02-22 00:33:22 -0500

    adjust footer py to pb for spacing issues. add in footer text

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -4,11 +4,15 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer className="bg-primary-100 py-16">
+    <footer className="bg-primary-100 pb-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
           <img src={Logo} alt="logo" />
-          <p className="my-5"></p>
+          <p className="my-5">
+            Don't hesitate to reach out to us if you have any questions or
+            concerns. We look forward to helping you on your journey to a
+            healthier and happier life!
+          </p>
           <p>
             ©{" "}
             <a
