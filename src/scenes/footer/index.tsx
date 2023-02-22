d6cc9dab0b95cdc0d82957f4cec502a54b756d4d
Justin Lam <justin.kh.lam@gmail.com>
2023-02-22 00:42:32 -0500

    adjust logo size. adjust margin sizing

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -4,10 +4,10 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer className="bg-primary-100 py-16">
+    <footer className="bg-primary-100 py-10">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
-        <div className="mt-16 basis-1/2 md:mt-0">
-          <img src={Logo} alt="logo" />
+        <div className="mt-10 basis-1/2 md:mt-0">
+          <img className="h-[200px]" src={Logo} alt="logo" />
           <p className="my-5">
             Don't hesitate to reach out to us if you have any questions or
             concerns. We look forward to helping you on your journey to a
@@ -26,7 +26,7 @@ const Footer = (props: Props) => {
         </div>
 
         {/* Links */}
-        <div className="mt-16 basis-1/4 md:mt-0">
+        <div className="mt-10 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
           <p className="my-5">Link1</p>
           <p className="my-5">Link2</p>
@@ -34,7 +34,7 @@ const Footer = (props: Props) => {
         </div>
 
         {/* Contact */}
-        <div className="mt-16 basis-1/4 md:mt-0">
+        <div className="mt-10 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
           <p>(416)-555-0100</p>
         </div>
