a59d8ed30cff7b4dab5c33d73f250299f35ff297
Justin Lam <justin.kh.lam@gmail.com>
2023-02-22 00:40:41 -0500

    adjust styling to keep links/contact us section in line with logo

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer className="bg-primary-100 pb-16">
+    <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
           <img src={Logo} alt="logo" />
