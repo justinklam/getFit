4b6c1dc3c150991cfbd96a675385b74a53dd0b0c
Justin Lam <justin.kh.lam@gmail.com>
2023-02-22 00:18:03 -0500

    add main container element. add additional elements for logos

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -3,7 +3,11 @@ import Logo from "@/assets/logo-no-bg-sm.png";
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex"></div>
+    </footer>
+  );
 };
 
 export default Footer;
