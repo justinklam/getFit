04f95ba08881b4ed433aba80600ddcbbe70e6968
Justin Lam <justin.kh.lam@gmail.com>
2023-02-22 00:26:48 -0500

    add in elements for copyright section with hyperlink to github

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -5,7 +5,24 @@ type Props = {};
 const Footer = (props: Props) => {
   return (
     <footer className="bg-primary-100 py-16">
-      <div className="justify-content mx-auto w-5/6 gap-16 md:flex"></div>
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img src={Logo} alt="logo" />
+          <p className="my-5"></p>
+          <p>
+            ©{" "}
+            <a
+              className="text-primary-500"
+              href="https://github.com/justinklam"
+            >
+              JKL
+            </a>{" "}
+            All Rights Reserved
+          </p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
     </footer>
   );
 };
